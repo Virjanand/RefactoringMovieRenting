fe2c5b882d6cfa76ee8376b288985f901d01657f
Virjanand <virjanand@virjanand.com>
2019-04-30 11:55:06 +0200

    Move getCharge to Price class and extract methods for each case

--- src/com/virjanand/refactoringMovieRenting/Price.java
+++ src/com/virjanand/refactoringMovieRenting/Price.java
@@ -1,5 +1,43 @@
 package com.virjanand.refactoringMovieRenting;
 
+import static com.virjanand.refactoringMovieRenting.Movie.CHILDRENS;
+import static com.virjanand.refactoringMovieRenting.Movie.NEW_RELEASE;
+import static com.virjanand.refactoringMovieRenting.Movie.REGULAR;
+
 abstract class Price {
     abstract int getPriceCode();
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                result = getChargeRegular(daysRented);
+                break;
+            case NEW_RELEASE:
+                result = getChargeNewRelease(daysRented);
+                break;
+            case CHILDRENS:
+                result = getChargeChildrens(daysRented);
+                break;
+        }
+        return result;
+    }
+
+
+    private double getChargeNewRelease(int daysRented) {
+        return (double) (daysRented * 3);
+    }
+    private double getChargeChildrens(int daysRented) {
+        double result = 1.5;
+        if (daysRented > 3)
+            result += (daysRented - 3) * 1.5;
+        return result;
+    }
+
+    private double getChargeRegular(int daysRented) {
+        double result = 2;
+        if (daysRented > 2)
+            result += (daysRented - 2) * 1.5;
+        return result;
+    }
 }
